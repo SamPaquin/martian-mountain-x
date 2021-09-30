bf212298e87bd2f0c35571d946aed3690e447b28
Sam Paquin <spaquin1114@gmail.com>
2021-09-29 20:53:42 -0500

    css updates

--- src/Pages/Start.js
+++ src/Pages/Start.js
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
-import "./Start.css"
+import "./Start.css";
 
 const Start = () => {
   return (
     <div className="start">
       <h1>Martian Mountain X</h1>
-      <button>
-        <Link to="/play">Let's Play!</Link>
-      </button>
+      <Link to="/play">
+        <button>Let's Play!</button>
+      </Link>
       <h2>Created by:</h2>
       <h2>Johny Chhantyal &amp; Sam Paquin</h2>
       <button>How to Play</button>
